7077aeb66affdb6c558e6da61472cf22edf0d0b6
Kim Youngsan <73068758+angoora@users.noreply.github.com>
2024-02-06 13:17:20 +0900

    샘플차트 주석제거

--- assets/js/publish.js
+++ assets/js/publish.js
@@ -29,119 +29,119 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 if (document.querySelectorAll(".chart").length > 0) {
-  // Highcharts.chart("sleep_chart", {
-  //   chart: {
-  //     type: "line",
-  //   },
-  //   title: {
-  //     text: "",
-  //   },
-  //   yAxis: {
-  //     title: "",
-  //     minorGridLineWidth: 0,
-  //     gridLineWidth: 0,
-  //     alternateGridColor: null,
-  //     plotBands: [
-  //       {
-  //         from: 0,
-  //         to: 2,
-  //         color: "#0A2647",
-  //         label: {
-  //           text: "깊은수면",
-  //           style: {
-  //             color: "#fff",
-  //           },
-  //         },
-  //       },
-  //       {
-  //         // Light breeze
-  //         from: 2,
-  //         to: 4,
-  //         color: "#144272",
-  //         label: {
-  //           text: "얕은수면",
-  //           style: {
-  //             color: "#fff",
-  //           },
-  //         },
-  //       },
-  //       {
-  //         // Gentle breeze
-  //         from: 4,
-  //         to: 6,
-  //         color: "#205295",
-  //         label: {
-  //           text: "렘수면",
-  //           style: {
-  //             color: "#fff",
-  //           },
-  //         },
-  //       },
-  //       {
-  //         // Moderate breeze
-  //         from: 6,
-  //         to: 8,
-  //         color: "#2C74B3",
-  //         label: {
-  //           text: "비수면",
-  //           style: {
-  //             color: "#fff",
-  //           },
-  //         },
-  //       },
-  //     ],
-  //   },
-  //   exporting: {
-  //     enabled: false,
-  //   },
+  Highcharts.chart("sleep_chart", {
+    chart: {
+      type: "line",
+    },
+    title: {
+      text: "",
+    },
+    yAxis: {
+      title: "",
+      minorGridLineWidth: 0,
+      gridLineWidth: 0,
+      alternateGridColor: null,
+      plotBands: [
+        {
+          from: 0,
+          to: 2,
+          color: "#0A2647",
+          label: {
+            text: "깊은수면",
+            style: {
+              color: "#fff",
+            },
+          },
+        },
+        {
+          // Light breeze
+          from: 2,
+          to: 4,
+          color: "#144272",
+          label: {
+            text: "얕은수면",
+            style: {
+              color: "#fff",
+            },
+          },
+        },
+        {
+          // Gentle breeze
+          from: 4,
+          to: 6,
+          color: "#205295",
+          label: {
+            text: "렘수면",
+            style: {
+              color: "#fff",
+            },
+          },
+        },
+        {
+          // Moderate breeze
+          from: 6,
+          to: 8,
+          color: "#2C74B3",
+          label: {
+            text: "비수면",
+            style: {
+              color: "#fff",
+            },
+          },
+        },
+      ],
+    },
+    exporting: {
+      enabled: false,
+    },
 
-  //   plotOptions: {
-  //     line: {
-  //       step: true,
-  //       lineWidth: 4,
-  //       showInLegend: false,
-  //       marker: {
-  //         enabled: false,
-  //       },
-  //       pointStart: 0,
-  //     },
-  //   },
-  //   series: [
-  //     {
-  //       name: "Hestavollane",
-  //       color: "#FFE15D",
-  //       data: [
-  //         "",
-  //         "",
-  //         7,
-  //         5,
-  //         7,
-  //         3,
-  //         1,
-  //         5,
-  //         1,
-  //         3,
-  //         7,
-  //         3,
-  //         3,
-  //         5,
-  //         3,
-  //         1,
-  //         1,
-  //         3,
-  //         5,
-  //         7,
-  //         7,
-  //         7,
-  //       ],
-  //     },
-  //   ],
-  //   navigation: {
-  //     menuItemStyle: {
-  //       fontSize: "10px",
-  //     },
-  //   },
-  // });
+    plotOptions: {
+      line: {
+        step: true,
+        lineWidth: 4,
+        showInLegend: false,
+        marker: {
+          enabled: false,
+        },
+        pointStart: 0,
+      },
+    },
+    series: [
+      {
+        name: "Hestavollane",
+        color: "#FFE15D",
+        data: [
+          "",
+          "",
+          7,
+          5,
+          7,
+          3,
+          1,
+          5,
+          1,
+          3,
+          7,
+          3,
+          3,
+          5,
+          3,
+          1,
+          1,
+          3,
+          5,
+          7,
+          7,
+          7,
+        ],
+      },
+    ],
+    navigation: {
+      menuItemStyle: {
+        fontSize: "10px",
+      },
+    },
+  });
 
   Highcharts.chart("chart1_1", {
     chart: {
@@ -504,152 +504,160 @@ if (document.querySelectorAll(".chart").length > 0) {
       },
     ],
   });
-  // Highcharts.chart('chart2_1', {
-  //   chart: {
-  //     type: 'line',
-  //     marginRight: 10,
-  //   },
+  Highcharts.chart("chart2_1", {
+    chart: {
+      type: "line",
+      marginRight: 10,
+    },
 
-  //   time: {
-  //     useUTC: false
-  //   },
-  //   height: '150px',
-  //   title: {
-  //     text: ''
-  //   },
-  //   accessibility: {
-  //     announceNewData: {
-  //       enabled: true,
-  //       minAnnounceInterval: 15000,
-  //       announcementFormatter: function (allSeries, newSeries, newPoint) {
-  //         if (newPoint) {
-  //           return 'New point added. Value: ' + newPoint.y;
-  //         }
-  //         return false;
-  //       }
-  //     }
-  //   },
+    time: {
+      useUTC: false,
+    },
+    height: "150px",
+    title: {
+      text: "",
+    },
+    accessibility: {
+      announceNewData: {
+        enabled: true,
+        minAnnounceInterval: 15000,
+        announcementFormatter: function (allSeries, newSeries, newPoint) {
+          if (newPoint) {
+            return "New point added. Value: " + newPoint.y;
+          }
+          return false;
+        },
+      },
+    },
 
-  //   xAxis: {
-  //     type: 'datetime',
-  //     tickPixelInterval: 150
-  //   },
+    xAxis: {
+      type: "datetime",
+      tickPixelInterval: 150,
+    },
 
-  //   yAxis: {
-  //     title: {
-  //       text: ''
-  //     },
-  //     plotLines: [{
-  //       value: 0,
-  //       width: 1,
-  //     }]
-  //   },
+    yAxis: {
+      title: {
+        text: "",
+      },
+      plotLines: [
+        {
+          value: 0,
+          width: 1,
+        },
+      ],
+    },
 
-  //   // tooltip: {
-  //   //   headerFormat: '<b>{series.name}</b><br/>',
-  //   //   pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}'
-  //   // },
+    // tooltip: {
+    //   headerFormat: '<b>{series.name}</b><br/>',
+    //   pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}'
+    // },
 
-  //   legend: {
-  //     enabled: false,
-  //   },
+    legend: {
+      enabled: false,
+    },
 
-  //   exporting: {
-  //     enabled: false,
-  //   },
+    exporting: {
+      enabled: false,
+    },
 
-  //   series: [{
-  //     name: 'Random data',
-  //     color: '#DC3535',
-  //     marker: false,
-  //     data: (function () {
-  //       // generate an array of random data
-  //       var data = [],
-  //         time = (new Date()).getTime(),
-  //         i;
+    series: [
+      {
+        name: "Random data",
+        color: "#DC3535",
+        marker: false,
+        data: (function () {
+          // generate an array of random data
+          var data = [],
+            time = new Date().getTime(),
+            i;
 
-  //       for (i = -19; i <= 0; i += 1) {
-  //         data.push({
-  //           x: time + i * 1000,
-  //           y: Math.random()
-  //         });
-  //       }
-  //       return data;
-  //     }())
-  //   }]
-  // });
-  // Highcharts.chart('chart2_2', {
-  //   chart: {
-  //     type: 'line',
-  //     marginRight: 10,
-  //   },
+          for (i = -19; i <= 0; i += 1) {
+            data.push({
+              x: time + i * 1000,
+              y: Math.random(),
+            });
+          }
+          return data;
+        })(),
+      },
+    ],
+  });
+  Highcharts.chart("chart2_2", {
+    chart: {
+      type: "line",
+      marginRight: 10,
+    },
 
-  //   time: {
-  //     useUTC: false
-  //   },
-  //   height: '150px',
-  //   title: {
-  //     text: ''
-  //   },
-  //   accessibility: {
-  //     announceNewData: {
-  //       enabled: true,
-  //       minAnnounceInterval: 15000,
-  //       announcementFormatter: function (allSeries, newSeries, newPoint) {
-  //         if (newPoint) {
-  //           return 'New point added. Value: ' + newPoint.y;
-  //         }
-  //         return false;
-  //       }
-  //     }
-  //   },
+    time: {
+      useUTC: false,
+    },
+    height: "150px",
+    title: {
+      text: "",
+    },
+    accessibility: {
+      announceNewData: {
+        enabled: true,
+        minAnnounceInterval: 15000,
+        announcementFormatter: function (allSeries, newSeries, newPoint) {
+          if (newPoint) {
+            return "New point added. Value: " + newPoint.y;
+          }
+          return false;
+        },
+      },
+    },
 
-  //   xAxis: {
-  //     type: 'datetime',
-  //     tickPixelInterval: 150
-  //   },
+    xAxis: {
+      type: "datetime",
+      tickPixelInterval: 150,
+    },
 
-  //   yAxis: {
-  //     title: {
-  //       text: ''
-  //     },
-  //     plotLines: [{
-  //       value: 0,
-  //       width: 1,
-  //     }]
-  //   },
+    yAxis: {
+      title: {
+        text: "",
+      },
+      plotLines: [
+        {
+          value: 0,
+          width: 1,
+        },
+      ],
+    },
 
-  //   // tooltip: {
-  //   //   headerFormat: '<b>{series.name}</b><br/>',
-  //   //   pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}'
-  //   // },
+    // tooltip: {
+    //   headerFormat: '<b>{series.name}</b><br/>',
+    //   pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}'
+    // },
 
-  //   legend: {
-  //     enabled: false,
-  //   },
+    legend: {
+      enabled: false,
+    },
 
-  //   exporting: {
-  //     enabled: false,
-  //   },
+    exporting: {
+      enabled: false,
+    },
 
-  //   series: [{
-  //     name: 'Random data',
-  //     color: '#68B984',
-  //     marker: false,
-  //     data: (function () {
-  //       // generate an array of random data
-  //       var data = [],
-  //         time = (new Date()).getTime(),
-  //         i;
+    series: [
+      {
+        name: "Random data",
+        color: "#68B984",
+        marker: false,
+        data: (function () {
+          // generate an array of random data
+          var data = [],
+            time = new Date().getTime(),
+            i;
 
-  //       for (i = -19; i <= 0; i += 1) {
-  //         data.push({
-  //           x: time + i * 1000,
-  //           y: Math.random()
-  //         });
-  //       }
-  //       return data;
-  //     }())
-  //   }]
-  // });
+          for (i = -19; i <= 0; i += 1) {
+            data.push({
+              x: time + i * 1000,
+              y: Math.random(),
+            });
+          }
+          return data;
+        })(),
+      },
+    ],
+  });
 }
